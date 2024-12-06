5582f49803c563ddb3fe84d50ed80c02b539226d
Prokofyev-Evg <Prokofyev.Evg@yandex.ru>
2024-12-06 18:16:28 +0300

    Create CV_4.py

---
+++ CV_4.py
@@ -0,0 +1,60 @@
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Conv2D, Flatten, Dense, GlobalAveragePooling2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications.resnet import ResNet50
+import numpy as np
+
+optimizer = Adam()
+
+def load_train(path):
+    datagen = ImageDataGenerator(
+        # horizontal_flip=True,
+        # vertical_flip=True,
+        # width_shift_range=0.2,
+        # height_shift_range=0.2,
+        # rotation_range=90,
+        validation_split=0.1,
+        rescale=1./255
+    )
+
+    train_datagen_flow = datagen.flow_from_directory(
+        path,
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='sparse',
+        subset='training',
+        seed=12345
+    )
+
+    return train_datagen_flow
+
+def create_model(input_shape):
+
+
+
+
+    backbone = ResNet50(input_shape=(150, 150, 3),
+                        weights='/datasets/keras_models/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5',
+                        include_top=False)
+    backbone.trainable = False
+    model = Sequential()
+    model.add(backbone)
+    model.add(GlobalAveragePooling2D())
+    # model.add(Dropout(0.2)) 
+    model.add(Dense(12, activation='softmax'))
+    model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', 
+              metrics=['acc']) 
+
+    return model
+
+def train_model(model, train_data, test_data, batch_size=None, epochs=20,
+               steps_per_epoch=None, validation_steps=None):
+    model.fit(train_data, 
+              validation_data=test_data,
+              batch_size=batch_size, epochs=epochs,
+              steps_per_epoch=steps_per_epoch,
+              validation_steps=validation_steps,
+              verbose=2, shuffle=True)
+
+    return model
