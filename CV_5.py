6b18c170cb4894fd72150ae9433c26a18e29d633
Prokofyev-Evg <Prokofyev.Evg@yandex.ru>
2024-12-10 16:16:29 +0300

    Update CV_5.py

--- CV_5.py
+++ CV_5.py
@@ -1,10 +1,12 @@
 import os
+import pandas as pd
 import numpy as np
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Conv2D, Flatten, Dense, AveragePooling2D, GlobalAveragePooling2D, Dropout
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications.resnet import ResNet50
+
 
 def load_train(path):
     labels_df = pd.read_csv(os.path.join(path, 'labels.csv'))
@@ -18,7 +20,7 @@ def load_train(path):
         target_size=(224, 224),
         x_col='file_name',
         y_col='real_age',
-        batch_size=16,
+        batch_size=32,
         class_mode='raw',
         subset='training',
         seed=12345
@@ -37,7 +39,7 @@ def load_test(path):
         target_size=(224, 224),
         x_col='file_name',
         y_col='real_age',
-        batch_size=16,
+        batch_size=32,
         class_mode='raw',
         subset='validation',
         seed=12345
@@ -47,21 +49,21 @@ def load_test(path):
 
 def create_model(input_shape):
     backbone = ResNet50(input_shape=input_shape,
-                        weights='/datasets/keras_models/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5',
                         include_top=False)
     model = Sequential()
     model.add(backbone)
-    model.add(Conv2D(filters=8, kernel_size=(3, 3), padding='same', input_shape=(150, 150, 3), activation='relu'))
+    model.add(Conv2D(filters=8, kernel_size=(3, 3), padding='same', input_shape=(224, 224, 3), activation='relu'))
+    model.add(Dropout(0.2))
     model.add(AveragePooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(filters=6, kernel_size=(3, 3), padding='same', input_shape=(150, 150, 3), activation='relu'))
+    model.add(Conv2D(filters=6, kernel_size=(3, 3), padding='same', input_shape=(224, 224, 3), activation='relu'))
     model.add(AveragePooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(filters=4, kernel_size=(3, 3), padding='same', input_shape=(150, 150, 3), activation='relu'))
+    model.add(Conv2D(filters=4, kernel_size=(3, 3), padding='same', input_shape=(224, 224, 3), activation='relu'))
     model.add(GlobalAveragePooling2D())
     model.add(Dense(1, activation='relu'))
     model.compile(optimizer=Adam(), loss='mean_squared_error', metrics=['mae']) 
     return model
 
-def train_model(model, train_data, test_data, batch_size=16, epochs=10,
+def train_model(model, train_data, test_data, batch_size=None, epochs=10,
                steps_per_epoch=None, validation_steps=None):
     model.fit(train_data, 
               validation_data=test_data,
